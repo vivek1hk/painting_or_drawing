5956ceb13b8a25d286061e56cb5fa465a208ba46
Vivek <vkrish9@uic.edu>
2017-05-04 13:31:03 -0500

    First commit

---
+++ static/script.js
@@ -0,0 +1,16 @@
+
+ console.log("bande guru");  
+$('#buttonId').click(function() {    
+ $.ajax({
+        url: your_url,
+        method: 'POST', // or another (GET), whatever you need
+        data: {
+            name: value, // data you need to pass to your function
+            click: True
+        }
+        success: function (data) {        
+            // success callback
+            // you can process data returned by function from views.py
+        }
+    });
+});
